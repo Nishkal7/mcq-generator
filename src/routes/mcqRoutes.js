366bf09aa92cb96a8002a0e3b445c26781ae1487
Nishkal7 <nishkal8463@gmail.com>
2024-05-17 20:07:51 +0530

    update api for mcq add/update

--- src/routes/mcqRoutes.js
+++ src/routes/mcqRoutes.js
@@ -23,4 +23,43 @@ router.post("/create", duplicateCheck, async (req, res) => {
   }
 });
 
+const checkItemInArray = (currentData, newItem) => {
+  const res = currentData.findIndex(
+    (currItem) => currItem.question === newItem.question
+  );
+  return res;
+};
+
+router.post("/update", async (req, res) => {
+  try {
+    const title = req.body.title;
+    const newData = req.body.data;
+    const record = await Mcq.exists({ title });
+    if (record) {
+      const result = await Mcq.findOne({ title });
+      const updatedAuthor = req.body.author ?? result.author;
+      newData.forEach((item) => {
+        const ind = checkItemInArray(result.data, item);
+        if (ind > -1) {
+          result.data.splice(ind, 1);
+        }
+      });
+
+      await Mcq.updateOne(
+        { title },
+        {
+          data: [...result.data, ...newData],
+          author: updatedAuthor,
+        }
+      );
+
+      res.send("Record updated Sucessfully");
+    } else {
+      res.status(404).send("Record not found");
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
 module.exports = router;
