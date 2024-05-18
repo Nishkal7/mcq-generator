065e7ad3f29a4a7e844a52e33de7ad51339c5464
Nishkal7 <nishkal8463@gmail.com>
2024-05-18 19:44:06 +0530

    excel upload logic and convert to JSON done

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const mcqRouter = require("./src/routes/mcqRoutes");
+const multer = require("multer");
+const excelToJson = require("convert-excel-to-json");
+const fs = require("fs-extra");
 
 const app = express();
+let upload = multer({ dest: "uploads/" });
 
 const dotenv = require("dotenv");
 dotenv.config({ path: "./.env" });
@@ -14,6 +18,34 @@ app.use("/mcq", mcqRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to my app!!");
+});
+
+app.post("/uploadFile", upload.single("file"), (req, res) => {
+  try {
+    console.log('req', req.file)
+    if (req.file?.filename === null || req.file?.filename === undefined) {
+      res.status(400).json("No File");
+    } else {
+      let filePath = "uploads\\" + req.file.filename;
+
+      const excelData = excelToJson({
+        sourceFile: filePath,
+        header: {
+          rows: 1,
+        },
+        columnToKey: {
+          A: "id",
+          B: "Question",
+          C: "Options",
+          D: "Answers",
+        },
+      });
+      fs.remove(filePath);
+      res.status(200).json(excelData);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 app.listen(process.env.PORT, () => {
